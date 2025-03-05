bdeced3dc6dcb2bdf77eb6e5ce5cead6f2d0457d
Marwan Atef <atefmarwan60@gmail.com>
2025-03-05 12:40:11 +0200

    floating bottomNavigationBar

--- lib/features/Home_screen/presentation/screens/home_screen.dart
+++ lib/features/Home_screen/presentation/screens/home_screen.dart
@@ -39,44 +39,47 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       body: tabs[currentIndex],
       bottomNavigationBar: SafeArea(
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(16),
-          child: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: AppTheme.gray,
-            elevation: 0,
-            showUnselectedLabels: false,
-            showSelectedLabels: false,
-            currentIndex: currentIndex,
-            onTap: (value) {
-              if (currentIndex != value) {
-                setState(() {
-                  currentIndex = value;
-                });
-              }
-            },
-            items: [
-              BottomNavigationBarItem(
-                icon: NavIcon(imageIcon: "home"),
-                activeIcon: NavIcon(imageIcon: "homeActive"),
-                label: "Home",
-              ),
-              BottomNavigationBarItem(
-                icon: NavIcon(imageIcon: "search"),
-                activeIcon: NavIcon(imageIcon: "searchActive"),
-                label: "Search",
-              ),
-              BottomNavigationBarItem(
-                icon: NavIcon(imageIcon: "browse"),
-                activeIcon: NavIcon(imageIcon: "browseActive"),
-                label: "Browse",
-              ),
-              BottomNavigationBarItem(
-                icon: NavIcon(imageIcon: "profile"),
-                activeIcon: NavIcon(imageIcon: "profileActive"),
-                label: "Profile",
-              ),
-            ],
+        child: Padding(
+          padding:const EdgeInsets.only(left: 9,right: 9,bottom: 9),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              backgroundColor: AppTheme.gray,
+              elevation: 0,
+              showUnselectedLabels: false,
+              showSelectedLabels: false,
+              currentIndex: currentIndex,
+              onTap: (value) {
+                if (currentIndex != value) {
+                  setState(() {
+                    currentIndex = value;
+                  });
+                }
+              },
+              items: [
+                BottomNavigationBarItem(
+                  icon: NavIcon(imageIcon: "home"),
+                  activeIcon: NavIcon(imageIcon: "homeActive"),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: NavIcon(imageIcon: "search"),
+                  activeIcon: NavIcon(imageIcon: "searchActive"),
+                  label: "Search",
+                ),
+                BottomNavigationBarItem(
+                  icon: NavIcon(imageIcon: "browse"),
+                  activeIcon: NavIcon(imageIcon: "browseActive"),
+                  label: "Browse",
+                ),
+                BottomNavigationBarItem(
+                  icon: NavIcon(imageIcon: "profile"),
+                  activeIcon: NavIcon(imageIcon: "profileActive"),
+                  label: "Profile",
+                ),
+              ],
+            ),
           ),
         ),
       ),
