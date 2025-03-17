0fe5163f65ed83f308329cfd5c862d64a8bd124a
ZahraaGenedy <zahraagenedy151@gmail.com>
2025-03-17 03:28:12 +0300

    edit tabBar (Watchlist , history) vesibility

--- lib/features/Profile/data/data_sources/FirebaseUtils.dart
+++ lib/features/Profile/data/data_sources/FirebaseUtils.dart
@@ -3,12 +3,10 @@ import '../../../onboarding/services/sharedpreferencekeys.dart';
 import '../../../onboarding/services/sharedpreferences.dart';
 
 class FirebaseUtils {
-  // مرجع لمجموعة التاريخ لكل مستخدم
   static CollectionReference getHistoryCollection() {
     return FirebaseFirestore.instance.collection("UserData");
   }
 
-  // دالة لإضافة ID للفيلم للـ array
   static Future<void> addMovieID(int movieId) async {
     final token = LocalStorageServices.getString(LocalStorageKeys.authToken);
     print("Token: $token");
@@ -16,7 +14,6 @@ class FirebaseUtils {
     if (token != null) {
       var userDoc = getHistoryCollection().doc(token);
 
-      // استرجاع الـ array الحالية
       DocumentSnapshot doc = await userDoc.get();
       List<dynamic> moviesID = [];
 
@@ -24,22 +21,19 @@ class FirebaseUtils {
         moviesID = List.from(doc['moviesID'] ?? []);
       }
 
-      // إزالة الـ ID لو موجود بالفعل
       moviesID.remove(movieId);
 
-      // إضافة الـ ID الجديد في أول الـ list
       moviesID.insert(0, movieId);
 
-      // تحديث الـ array في Firebase
       await userDoc
           .set(
             {'moviesID': moviesID},
-            SetOptions(merge: true), // ➡️ دمج البيانات بدون حذف
+            SetOptions(merge: true),
           )
           .then((_) => print("✅ Movie ID $movieId added to Firebase"))
-          .catchError((error) => print("❌ Failed to add movie ID: $error"));
+          .catchError((error) => print("Failed to add movie ID: $error"));
     } else {
-      print("❌ Token is null, cannot save data");
+      print("Token is null, cannot save data");
     }
   }
 
@@ -55,18 +49,16 @@ class FirebaseUtils {
 
         if (doc.exists && doc.data() != null) {
           List<dynamic> moviesID = List.from(doc['moviesID'] ?? []);
-          return moviesID
-              .map((id) => id as int)
-              .toList(); // تحويل الـ dynamic لـ int
+          return moviesID.map((id) => id as int).toList();
         } else {
-          return []; // لو الـ doc مش موجود نرجع ليست فاضية
+          return [];
         }
       } catch (e) {
-        print("❌ Failed to fetch movie IDs: $e");
-        return []; // في حالة الخطأ نرجع ليست فاضية
+        print("Failed to fetch movie IDs: $e");
+        return [];
       }
     } else {
-      print("❌ Token is null, cannot retrieve data");
+      print("Token is null, cannot retrieve data");
       return [];
     }
   }
@@ -92,10 +84,10 @@ class FirebaseUtils {
             {'moviesID': moviesID},
             SetOptions(merge: true),
           )
-          .then((_) => print("✅ Movie ID $movieId removed from Firebase"))
-          .catchError((error) => print("❌ Failed to remove movie ID: $error"));
+          .then((_) => print("Movie ID $movieId removed from Firebase"))
+          .catchError((error) => print("Failed to remove movie ID: $error"));
     } else {
-      print("❌ Token is null, cannot remove data");
+      print("Token is null, cannot remove data");
     }
   }
 }
